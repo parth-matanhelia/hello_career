d14da792f3a43596887dcdf33768d1dcb966bc15
GNVageesh <vageeshgn2005@gmail.com>
2021-07-24 23:28:21 +0530

    Something new in th register page

--- pages/register.js
+++ pages/register.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import styles from '../styles/Register.module.css'
 
 const Register = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className={styles.sidebar}>
+                <h1>Hello world</h1>
+                <p>lorem ipsum dolor sit amet, consectetur adip</p>
+            </div>
+        </>
     )
 }
 
