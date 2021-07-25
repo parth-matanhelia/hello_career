3fb43d5814c61ebd59e23534bbb98e883b2fc363
GNVageesh <vageeshgn2005@gmail.com>
2021-07-25 10:12:20 +0530

    some form styling to login page

--- pages/login.js
+++ pages/login.js
@@ -9,17 +9,20 @@ const Login = () => {
             {/* <Navbar /> */}
             <div className={styles.loginPart}>
                 <div className={styles.group}>
-                    <div className="leftSide">
-                        <Image src="/images/login2.jpg" width={500} height={500} />
+                    <div className={styles.leftSide}>
+                        <Image src="/images/login2.jpg" width={500} height={500} alt="LoginImage" />
                     </div>
                     <div className={styles.rightSide}>
-                        <form action="">
-                            <input className={styles.field} type="email" name="email" id="email" placeholder="Email ID" />
-                            <br />
-                            <input className={styles.field} type="password" name="pass" id="pass" placeholder="Password" />
-                            <br />
-                            <button type="submit">Login</button>
-                        </form>
+                        <div className={styles.container}>
+                            <h1 className={styles.logTitle}>Welcome Back Achiever</h1>
+                            <form className={styles.form} action="">
+                                <input className={styles.field} type="email" name="email" id="email" placeholder="Email ID" />
+                                <br />
+                                <input className={styles.field} type="password" name="pass" id="pass" placeholder="Password" />
+                                <br />
+                                <button className={styles.btnLogin} type="submit">Login</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
