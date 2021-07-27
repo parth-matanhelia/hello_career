1d3eaa0ff81f9e44cae374aec10913fd106e99d0
parth-matanhelia <83057782+parth-matanhelia@users.noreply.github.com>
2021-07-27 11:14:56 +0530

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,14 +1,59 @@
 import React from 'react'
 import styles from '../styles/Register.module.css'
+import Image from 'next/image'
+import Navbar from '../components/Navbar'
 
 const Register = () => {
     return (
-        <>
-            <div className={styles.sidebar}>
-                <h1>Hello world</h1>
-                <p>lorem ipsum dolor sit amet, consectetur adip</p>
+        <div>
+           <Navbar />
+       
+                <div className={styles.reg}>
+                    <h1>Register</h1>
+                </div>
+
+                    <div className={styles.illu}>
+    
+                        <Image src="/images/illu.png" alt="illustration" />
+                    </div>
+
+
+
+                <div className={stlyes.box}>
+                    </div>
+
+            <div className={styles.username}>
+                  <input type="text" className={styles.inp} placeholder="Username">
             </div>
-        </>
+
+
+
+                    <div className={styles.phno}>
+                          <input type="number" placeholder="Phone Number" className={styles.inp}>
+                    </div>
+
+                                <div className={styles.email}>
+                                     <input type="email" placeholder="email" className={styles.inp}  >
+                                </div>
+
+                                            <div className={syles.password}>
+                                                     <input type="password"  className={styles.inp} placeholder="Password">
+                                            </div>
+
+                                                        <div className={styles.confirm}>
+                                                                 <input type="password" className={styles.inp} placeholder="Confirm Password">
+                                                       
+                                                         </div>
+    
+
+<button className={styles.signupbutton}>
+<a href="hellocareer.co.in/joined" >
+    <p>Join Us</p>
+</a>
+</button>
+
+
+        </div>
     )
 }
 
